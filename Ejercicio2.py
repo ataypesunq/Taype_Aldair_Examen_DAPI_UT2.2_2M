e0105be86cd5072d7940a77e123a296ea2571d94
ataypesunq <ataypesunq@educacion.navarra.es>
2025-01-17 09:37:11 +0100

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+datos = {}
+nombre = int(input('/nMenu:'
+               '(1) Añadir alumno/a)' 
+               '(2) Numero de aprobados'
+               '(3) Mostrar todo el alumnado'
+               ('Introduce 1, 2 o 3: ')))
+while True:
+    if nombre == 1:
+        datos[nombre] = None
+        
